bf7b696243de74c13001f80909df443bb4073a33
sashinexists <myself@sashinexists.com>
2021-03-21 05:45:45 +1100

    refining active state and rendering

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,56 +5,91 @@ import Board from './Board'
 import { Move, makeMove } from '../shared/Move'
 import '../styles/app.css'
 
-const initialState = ():Array<Array<square>> => new Array(9).fill([]).map(board => {return new Array(9).fill(square["blank"])});
+const initialState = (): Array<Array<square>> =>
+  new Array(9).fill([]).map((board) => {
+    return new Array(9).fill(square['blank'])
+  })
 const model: Array<Move> = []
 
 function App() {
   const [game, setGame] = useState(model)
 
   function takeTurn(board: number, position: number) {
-    const isXTurn = game.length > 0 ? game[game.length - 1].isXTurn : false
-    const move = makeMove(board, position, !isXTurn)
-    setGame((prevState) => [...prevState, move])
-  }
-
-  function checkBoardWin(board:number):square {
-    const winStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,5],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-    const currentBoard =calculateCurrentState()[board];
-    let winner:square = square["blank"];
-    winStates.forEach((winState) => {
-      if(currentBoard[winState[0]]===currentBoard[winState[1]]
-        &&currentBoard[winState[1]]===currentBoard[winState[2]]
-        &&currentBoard[winState[2]]!==square['blank']) {
-          winner = currentBoard[winState[0]];
-        }
-    })
-    return winner;
-  }
-
-  function getActiveBoard():activeState {
-    if(game.length>0 && checkBoardWin(game[game.length-1].position)===square['blank']) {
-      return game[game.length-1].position;
-    } else {
-      return activeState["all"];
+    if (isValidMove(board,position)) {
+      const isXTurn = game.length > 0 ? game[game.length - 1].isXTurn : false
+      const move = makeMove(board, position, !isXTurn)
+      setGame((prevState) => [...prevState, move])
     }
   }
 
-  function checkGameWin() {//work to do here
-    const winStates = [[0,1,2],[3,4,5],[6,7,8],[0,3,5],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  function isValidMove(board: number, position: number) {
+    return checkBoardWin(board)===square['blank'] && (getActiveBoard() === board || getActiveBoard() === activeState["all"]);
+  }
+
+  function checkBoardWin(board: number): square {
+    const winStates = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 5],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    const currentBoard = calculateCurrentState()[board]
+    let winner: square = square['blank']
+    winStates.forEach((winState) => {
+      if (
+        currentBoard[winState[0]] === currentBoard[winState[1]] &&
+        currentBoard[winState[1]] === currentBoard[winState[2]] &&
+        currentBoard[winState[2]] !== square['blank']
+      ) {
+        winner = currentBoard[winState[0]]
+      }
+    })
+    return winner
+  }
+
+  function getActiveBoard(): activeState {
+    if (
+      game.length > 0 &&
+      checkBoardWin(game[game.length - 1].position) === square['blank']
+    ) {
+      return game[game.length - 1].position
+    } else {
+      return activeState['all']
+    }
+  }
+
+  function checkGameWin() {
+    //work to do here
+    const winStates = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 5],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
     //reduce all boards with checkboardwin function to a winning boards then do the same winstates for each on this
-    const gameOvers = game.reduce((finishedGames:Array<number>,turn)=> {
-      const currentBoard =calculateCurrentState()[turn.board];
+    const gameOvers = game.reduce((finishedGames: Array<number>, turn) => {
+      const currentBoard = calculateCurrentState()[turn.board]
       winStates.forEach((winState) => {
-        if(currentBoard[winState[0]]===currentBoard[winState[1]]
-          &&currentBoard[winState[1]]===currentBoard[winState[2]]
-          &&currentBoard[winState[1]]!==square['blank']) {
-            finishedGames = [...finishedGames, turn.board];
-          } else {
-            finishedGames = [...finishedGames]
-          }
+        if (
+          currentBoard[winState[0]] === currentBoard[winState[1]] &&
+          currentBoard[winState[1]] === currentBoard[winState[2]] &&
+          currentBoard[winState[1]] !== square['blank']
+        ) {
+          finishedGames = [...finishedGames, turn.board]
+        } else {
+          finishedGames = [...finishedGames]
+        }
       })
-      return finishedGames;
-    },[])
+      return finishedGames
+    }, [])
   }
 
   function calculateCurrentState(): Array<Array<square>> {
@@ -70,7 +105,16 @@ function App() {
   return (
     <div className="game">
       {calculateCurrentState().map((board, i) => {
-        return <Board key={i} position={i} board={board} isActive={i===getActiveBoard()} winner={checkBoardWin(i)} takeTurn={takeTurn} />
+        return (
+          <Board
+            key={i}
+            position={i}
+            board={board}
+            isActive={i === getActiveBoard() || activeState["all"] === getActiveBoard()}
+            winner={checkBoardWin(i)}
+            takeTurn={takeTurn}
+          />
+        )
       })}
     </div>
   )
